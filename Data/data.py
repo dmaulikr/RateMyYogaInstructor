0289f1843da7744008084f6b1db48d3ee22a26d1
Vitaliy <vmatiyash@gmail.com>
2013-07-29 23:27:50 -0400

    changed yogi key to be a generated string. modified json-file structure

--- Data/data.py
+++ Data/data.py
@@ -1,61 +1,62 @@
 #!/usr/bin/env python
 
-import json, sys
+import json, sys, string, random
 
 def jsonTheYogaGirls():
-	data           =  open("data.txt", "r")
-	output         = open("data_after_python.txt", 'w+')
-	yogaData       = []
-	yogaInstructor = {}
-	firstName      = {}
-	lastName       = {}
-	city           = {}
-	state          = {}
-	country        = {}
-	count_line     = 0
+  data           =  open("data.txt", "r")
+  output         = open("data_after_python.txt", 'w+')
+  yogaData       = []
+  yogaInstructor = {}
+  firstName      = {}
+  lastName       = {}
+  city           = {}
+  state          = {}
+  country        = {}
+  count_line     = 0
 
 
-	for line in data:
-		if len(line) == 2:
-			yogaInstructor['yogi'] = firstName, lastName, city, state, country
-			yogaData.append(yogaInstructor)
+  for line in data:
+    if len(line) == 2:
+      keyID = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in range(20))
+      yogaInstructor[keyID] = firstName, lastName, city, state, country
+      yogaData.append(yogaInstructor)
 
-			yogaInstructor = dict()
-			firstName 	   = dict()
-			lastName       = dict()
-			city           = dict()
-			state          = dict()
-			country        = dict()
-			count_line     = 0
-		else:
-			if count_line == 0:
-				key = 'firstName'
-				value = line
-				firstName[key] = value
-			if count_line == 1:
-				key = 'lastName'
-				value = line
-				lastName[key] = value
-			if count_line == 2:
-				key = 'city'
-				value = line
-				city[key] = value
-			if count_line == 3:
-				key = 'state'
-				value = line
-				state[key] = value
-			if count_line == 4:
-				key = 'country'
-				value = line
-				country[key] = value
+      yogaInstructor = dict()
+      firstName      = dict()
+      lastName       = dict()
+      city           = dict()
+      state          = dict()
+      country        = dict()
+      count_line     = 0
+    else:
+      if count_line == 0:
+        key = 'firstName'
+        value = line
+        firstName[key] = value
+      if count_line == 1:
+        key = 'lastName'
+        value = line
+        lastName[key] = value
+      if count_line == 2:
+        key = 'city'
+        value = line
+        city[key] = value
+      if count_line == 3:
+        key = 'state'
+        value = line
+        state[key] = value
+      if count_line == 4:
+        key = 'country'
+        value = line
+        country[key] = value
 
-			count_line += 1
+      count_line += 1
 
-	for i in yogaData:
-		print i
+  for i in yogaData:
+    print i
 
-	output.writelines(["%s" % i for i in yogaData])
-		
+  output.writelines(["%s" % i for i in yogaData])
+    
 
 jsonTheYogaGirls()
 
